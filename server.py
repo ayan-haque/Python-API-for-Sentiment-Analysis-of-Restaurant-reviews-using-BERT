d02dc51af97a6b3459989746f137bdc944691544
Md Ayan Haque <110412621+ayan-haque@users.noreply.github.com>
2023-07-04 14:00:37 +0530

    server.py

--- server.py
+++ server.py
@@ -1,17 +1,13 @@
 import sys
 sys.path.append("<your_custom_path>/Python-API-for-Sentiment-Analysis-of-Restaurant-reviews-using-BERT")
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 CORS(app)
 
 from main_file import Restaurant_Reviews
-
 Restaurant_Reviews_Obj = Restaurant_Reviews()
-
 
 @app.route('/')
 def Test_check():
@@ -23,7 +19,7 @@ def Test_check():
 @app.route('/validate', methods = ['POST'])
 def validate_review():
     if request.method == 'POST':
-        data = request.get_json()  # data = {"input_data": "It was delicious"}
+        data = request.get_json()
         print(data) # Sample print
         results = Restaurant_Reviews_Obj.main(data)
         print(results)
@@ -31,5 +27,4 @@ def validate_review():
 
 if __name__ == '__main__':
     app.debug = True
-    # serve(app, host = '0.0.0.0', port = 8080)
-    app.run(threaded=True, host="127.0.0.1", port=5000) #runs the application on that local particular development server
+    app.run(threaded=True, host="127.0.0.1", port=5000)
